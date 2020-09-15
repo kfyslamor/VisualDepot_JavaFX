d865f604613a6edc77512886bc0a922c5b0fcb8c
kfypurp <41699598+kfyslamor@users.noreply.github.com>
2020-09-15 23:11:04 +0300

    Add files via upload

--- VisualDepot_JavaFX/src/sample/Controller.java
+++ VisualDepot_JavaFX/src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    FXMLLoader fxmlLoader = new FXMLLoader();//creating FXMLLoader object
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Loading user data...");
+    }
 }
